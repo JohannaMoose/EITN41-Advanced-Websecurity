145af591ab90f13f5185c5f8e4cb302528b18fe3
Johanna Petersson <min.mail@johannap.se>
2016-11-08 13:09:14 +0100

    Assignment 1.B.1 started

---
+++ HA1/LuhnAlgorithm/LuhnAlgorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LuhnAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to a Luhn Algorithm to find the missing number");
+            Console.WriteLine("Please enter or paste in the number/numbers to analyze: ");
+            var input = Console.ReadLine();
+            var numbers = input.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
